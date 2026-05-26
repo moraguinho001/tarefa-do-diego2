9627ac662a8c41036c65b897d0f23f555bbd52a5
moraguinho001 <motta.mariane@escola.pr.gov.br>
2026-05-26 08:49:34 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,11 +1,22 @@
-const input   = require("readline-sync")
+const input = require("readline-sync");
 let num1 = input.questionInt(`digite o primeiro numero:`)
-let num2 = input.questionInt(`digite o segundo numero:nod`)
-console.log(`${num1+num2}`)
-console.log(`${num1-num2}`)
-console.log(`${num1*num2}`)
-console.log(`${num1/num2}`)
+let num2 = input.questionInt(`digite o segundo numero:`)
+function somar(num1, num2) {
+return num1 + num2
+}
+function subtrair(num1, num2){
+    return num1 - num2
+}
+function multiplicacao(num1, num2){
+    return num1 * num2
+}
+function divisao(num1, num2){
+    return num1/num2
+}
 if(num2===0){
     console.log(`Erro: divisao por zero`)
 }
-console.log(`os resultados são: ${num1+num2}, ${num1-num2}, ${num1*num2}, ${num1/num2}`)
+console.log(somar(num1, num2))
+console.log(subtrair(num1, num2))
+console.log(multiplicacao(num1, num2))
+console.log(divisao(num1, num2))
